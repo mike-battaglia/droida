7238d40e0611389ae9be1a4536a8b52638527130
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2024-09-20 15:36:58 -0500

    Update main.php

--- main.php
+++ main.php
@@ -2,9 +2,12 @@
 /*
 Plugin Name: AI Art Description
 Description: Generates AI descriptions for artworks upon product publishing.
-Version: 1.2
+Version: 1.3
 Author: Sterling Digital
 */
 
-// Include general functions
+require_once plugin_dir_path(__FILE__) . 'includes/description-generator.php';
+require_once plugin_dir_path(__FILE__) . 'includes/auto-generate.php';
 require_once plugin_dir_path(__FILE__) . 'includes/settings.php';
+require_once plugin_dir_path(__FILE__) . 'includes/bulk-editing.php';
+require_once plugin_dir_path(__FILE__) . 'includes/add-edit-product.php';
