210b6372ba726fba1dfb06edb8eb4993292d6bed
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2024-09-20 15:08:32 -0500

    Create main.php

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: AI Art Description
+Description: Generates AI descriptions for artworks upon product publishing.
+Version: 1.2
+Author: Sterling Digital
+*/
+
+// Include general functions
+require_once plugin_dir_path(__FILE__) . 'includes/settings.php';
